9d958c1320e84306dc6b59dfaea3e147434897f0
Seeker <meaningseeking@protonmail.com>
2020-09-09 10:48:08 -0700

    Create filter-linux.rb

---
+++ filter-linux.rb
@@ -0,0 +1,9 @@
+path = ARGV[1] || "linuxbrew-core.txt"
+File.read(path).lines.each do |line|
+  m = line.match(/^([^ ]+) : [^ ]+ ==> [^ ]+$/)
+  next if m.nil?
+  name = m.captures.first
+  formula = Formulary.factory(name)
+  next unless formula.requirements.any? { |r| r.is_a?(LinuxRequirement) }
+  puts line
+end
