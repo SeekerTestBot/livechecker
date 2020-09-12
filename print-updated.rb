fc63629dd3e7ae9877b9a7ee82fa6ea66da86ac9
Seeker <meaningseeking@protonmail.com>
2020-09-11 17:26:34 -0700

    Update print-updated.rb

--- print-updated.rb
+++ print-updated.rb
@@ -3,24 +3,25 @@ livecheck_command = ARGV[1] || "livecheck"
 
 IO.popen([HOMEBREW_BREW_FILE, livecheck_command, "--newer-only", "--quiet"]) { |brew_io|
   livecheck_output = brew_io.read.strip
+  puts livecheck_output
 
-  new_hash = {}
+  # new_hash = {}
 
-  livecheck_output.lines.each do |line|
-    m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
-    next if m.nil?
-    fc_name = m.captures.first
-    new_hash[fc_name] = line.strip
-  end
+  # livecheck_output.lines.each do |line|
+  #   m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
+  #   next if m.nil?
+  #   fc_name = m.captures.first
+  #   new_hash[fc_name] = line.strip
+  # end
 
-  original_lines.each do |old_line|
-    m = old_line.match(/^(.+?) : (.+?) ==> (.+)$/)
-    if m.nil?
-      puts old_line
-      next
-    end
-    fc_name = m.captures.first
-    new_line = new_hash[fc_name]
-    puts new_line if new_line
-  end
+  # original_lines.each do |old_line|
+  #   m = old_line.match(/^(.+?) : (.+?) ==> (.+)$/)
+  #   if m.nil?
+  #     puts old_line
+  #     next
+  #   end
+  #   fc_name = m.captures.first
+  #   new_line = new_hash[fc_name]
+  #   puts new_line if new_line
+  # end
 }
