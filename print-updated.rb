4ea3b940a053c930fd1c7d10cdfe8427fc466568
Seeker <meaningseeking@protonmail.com>
2020-09-11 17:18:26 -0700

    Update print-updated.rb

--- print-updated.rb
+++ print-updated.rb
@@ -5,22 +5,23 @@ livecheck_command = ARGV[1] || "livecheck"
 
 IO.popen([HOMEBREW_BREW_FILE, livecheck_command, "--newer-only", "--quiet", "--json"]) { |brew_io|
   json_string = brew_io.read.strip
+  puts json_string
 
-  livecheck_array = JSON.parse(json_string)
+#   livecheck_array = JSON.parse(json_string)
 
-  new_hash = {}
+#   new_hash = {}
 
-  livecheck_array.each do |fc_hash|
-    if fc_hash["version"]["outdated"] || fc_hash["version"]["newer_than_upstream"]
-      fc_name = fc_hash["formula"] || fc_hash["cask"]
-      new_hash[fc_name] = "#{fc_name} : #{fc_hash["version"]["current"]} ==> #{fc_hash["version"]["latest"]}"
-    end
-  end
+#   livecheck_array.each do |fc_hash|
+#     if fc_hash["version"]["outdated"] || fc_hash["version"]["newer_than_upstream"]
+#       fc_name = fc_hash["formula"] || fc_hash["cask"]
+#       new_hash[fc_name] = "#{fc_name} : #{fc_hash["version"]["current"]} ==> #{fc_hash["version"]["latest"]}"
+#     end
+#   end
 
-  puts original_lines.map { |line|
-    m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
-    next line if m.nil?
-    fc_name = m.captures.first
-    new_hash[fc_name]
-  }.compact
+#   puts original_lines.map { |line|
+#     m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
+#     next line if m.nil?
+#     fc_name = m.captures.first
+#     new_hash[fc_name]
+#   }.compact
 }
