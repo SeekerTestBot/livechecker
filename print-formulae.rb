4ed2e10c9c7e305a4606ac54053913d0f45bdd84
Seeker <meaningseeking@protonmail.com>
2020-09-13 12:39:46 -0700

    Update print-formulae.rb

--- print-formulae.rb
+++ print-formulae.rb
@@ -1,12 +1,8 @@
 original_lines = File.read(ARGV[0] || "homebrew-core.txt").lines
-no_formulae = true
 
 original_lines.each do |line|
   m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
   next if m.nil?
-  no_formulae = false
   fc_name = m.captures.first
   puts fc_name
 end
-
-exit 1 if no_formulae
