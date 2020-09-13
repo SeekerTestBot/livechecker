5b53f7b80efce6d0632b8d7174665a88b029ba28
Seeker <meaningseeking@protonmail.com>
2020-09-13 10:07:34 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Usage: bash $GITHUB_WORKSPACE/update.sh languages.txt https://gist.github.com/SeekerTestBot/6f87957bdeecb59238ffd770e3d36431/raw
+# Usage: bash update.sh languages.txt https://gist.github.com/SeekerTestBot/6f87957bdeecb59238ffd770e3d36431/raw
 curl -o $1 -L $2
-brew ruby $GITHUB_WORKSPACE/print-formulae.rb $1 > $HOME/.brew_livecheck_watchlist
+brew ruby print-formulae.rb $1 > $HOME/.brew_livecheck_watchlist
 if [ $? -eq 0 ]; then
   if [ ! -z "$3" ]; then
     brew tap $3
