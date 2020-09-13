e24617d5a14f2adccae8bdb56fc5bec5602ace4a
Seeker <meaningseeking@protonmail.com>
2020-09-13 09:54:39 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Usage: bash $GITHUB_WORKSPACE/update.sh languages.txt SeekerTestBot/6f87957bdeecb59238ffd770e3d36431
-curl -o $1 -L https://gist.github.com/$2/raw
+# Usage: bash $GITHUB_WORKSPACE/update.sh languages.txt https://gist.github.com/SeekerTestBot/6f87957bdeecb59238ffd770e3d36431/raw
+curl -o $1 -L $2
 brew ruby $GITHUB_WORKSPACE/print-formulae.rb $1 > $HOME/.brew_livecheck_watchlist
 if [ $? -eq 0 ]; then
   brew livecheck --newer-only --quiet > $1 || true
