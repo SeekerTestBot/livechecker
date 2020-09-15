e3ff47f7c8d1c9704766d3c231e467164d9b99c3
Seeker <meaningseeking@protonmail.com>
2020-09-15 11:18:30 -0700

    Update print-formulae.rb

--- print-formulae.rb
+++ print-formulae.rb
@@ -1,8 +1,17 @@
-original_lines = File.read(ARGV[0] || "homebrew-core.txt").lines
+original_lines = File.read(ARGV.first || "homebrew-core.txt").lines
 
 original_lines.each do |line|
   m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
   next if m.nil?
   fc_name = m.captures.first
+  begin
+    if ARGV.include?("--casks") || ARGV.include?("--cask")
+      Cask::CaskLoader.load(fc_name)
+    else
+      Formulary.factory(fc_name)
+    end
+  rescue Cask::CaskUnavailableError, FormulaUnavailableError
+    next
+  end
   puts fc_name
 end
