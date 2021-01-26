645faa3558d1c523eb26ebcb59011157247db181
Seeker <meaningseeking@protonmail.com>
2021-01-25 23:43:29 -0800

    Update print-casks.rb

--- print-casks.rb
+++ print-casks.rb
@@ -1,14 +1,14 @@
 original_lines = File.read(ARGV.first || "homebrew-cask.txt").lines
-tap = ARGV.second || "homebrew/cask"
+tap = ARGV.second || ENV["LIVECHECKER_TAP"] || "homebrew/cask"
 
 original_lines.each do |line|
   m = line.match(/^(.+?) : (.+?) ==> (.+)$/)
   next if m.nil?
-  fc_name = m.captures.first
+  fc_name = m[1]
   begin
     Cask::CaskLoader.load(fc_name)
   rescue Cask::CaskUnavailableError
     next
   end
-  puts "#{tap}/#{fc_name}"
+  puts "#{tap}/#{fc_name}#{" : #{m[2]} ==> #{m[3]}" if ENV["LIVECHECKER_TAP"]}"
 end
